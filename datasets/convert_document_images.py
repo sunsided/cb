c7e28ac62b1cd6771cd97f078cd70614d6e6d6b6
Faiz Ul Wahab <faiz@nyris.io>
2018-10-28 20:22:18 +0100

    update for dataset in progress

--- datasets/convert_document_images.py
+++ datasets/convert_document_images.py
@@ -217,5 +217,12 @@ def run(dataset_dir):
   dataset_utils.write_label_file(labels_to_class_names, dataset_dir)
   print('\nFinished converting the documents dataset!')
 
+def main(_):
+  if not FLAGS.dataset_name:
+    raise ValueError('You must supply the dataset name with --dataset_name')
+  if not FLAGS.dataset_dir:
+    raise ValueError('You must supply the dataset directory with --dataset_dir')
+  run(FLAGS.dataset_dir)
+
 if __name__ == '__main__':
   tf.app.run()
